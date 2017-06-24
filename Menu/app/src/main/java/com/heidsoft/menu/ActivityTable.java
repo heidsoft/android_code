a3c2d957f41adc6e6edad4f22d32e7d4b8bdc225
heidsoft <heidsoft@sina.com>
2017-06-24 23:04:16 +0800

    Update 测试intent 意图 显示跳转活动

---
+++ Menu/app/src/main/java/com/heidsoft/menu/ActivityTable.java
@@ -0,0 +1,14 @@
+package com.heidsoft.menu;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTable extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+    }
+}
--- Menu/app/src/main/java/com/heidsoft/menu/MainActivity.java
+++ Menu/app/src/main/java/com/heidsoft/menu/MainActivity.java
@@ -1,13 +1,27 @@
 package com.heidsoft.menu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout);
+        setContentView(R.layout.activity_main);
+
+
+        Button button = (Button)findViewById(R.id.bn1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ActivityTable.class);
+                startActivity(intent);
+            }
+        });
     }
 }
